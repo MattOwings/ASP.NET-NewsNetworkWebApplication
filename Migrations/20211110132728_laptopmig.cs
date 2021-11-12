471a9eb2a734d8a5c0bcb5592af90eb7197e836f
MattOwings <mattalanowings@gmail.com>
2021-11-11 18:09:16 -0600

    in class updates

---
+++ Migrations/20211110132728_laptopmig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FinalProject_BloggingNewsSite.Migrations
+{
+    public partial class laptopmig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
